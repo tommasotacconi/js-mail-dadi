3e52cf35eccfbaa2d5f1eda9fb6a1fe36bb4bf97
tommmasotacconi <tommaso.tacconi@gmail.com>
2024-08-19 13:43:48 +0200

    dices' numbers result

--- jscript/script.js
+++ jscript/script.js
@@ -1,1 +1,11 @@
-console.log('OK JS')
+/* ESERCIZIO 1 - Gioco dei dadi
+1. Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+2. Stabilire il vincitore, in base a chi fa il punteggio più alto.
+3. Stampiamo in pagina i due tiri e il risultato
+ */
+
+//1.
+var playerOneDice = Math.floor(Math.random() * 6) + 1; //(0-5), +1, (1-6)
+var playerTwoDice = Math.floor(Math.random() * 6) + 1;
+
+console.log('playerOneDice', playerOneDice, ',', 'playerTwoDice', playerTwoDice)
